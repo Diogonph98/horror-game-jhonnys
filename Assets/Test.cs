f48ce270e4b2d5def22ffa523604e8d1678b63b2
AstroJustice <152734073+AstroJustice@users.noreply.github.com>
2023-12-02 21:45:33 +0000

    commentzz

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Start is called before the first frame update   JUSTICE IS 18
     void Start()
     {
         
